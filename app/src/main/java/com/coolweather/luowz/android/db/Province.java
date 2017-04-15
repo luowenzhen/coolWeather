2345aef585a09a06fbeb59d0c1328a68fac6cc13
luowz <luo.w.z@foxmail.com>
2017-04-15 11:33:01 +0800

    加入创建数据库和表的各项配置。

---
+++ app/src/main/java/com/coolweather/luowz/android/db/Province.java
@@ -0,0 +1,41 @@
+package com.coolweather.luowz.android.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by luowz on 2017/4/15.
+ * 记录省的信息
+ */
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+
+
+}
